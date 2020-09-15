4a68d7c1b20b69acaeabd52b1c834581c0847da7
JihoonJang <ljjk159@gmail.com>
2020-09-15 15:11:46 +0900

    make_input revised

--- RBTree_kColor/make_input.cpp
+++ RBTree_kColor/make_input.cpp
@@ -13,23 +13,9 @@ int RANDOM_MAX = 100000000;
 std::mt19937 gen(time(NULL));
 std::uniform_real_distribution<double> dis(0, 1); //uniform distribution between 0 and 1
 
-const int getRandInt()
+const int getRandInt(const int _max)
 {
-    return dis(gen) * RANDOM_MAX;
-}
-
-const double getRandDouble()
-{
-    return dis(gen);
-}
-
-template <typename S>
-auto select_random(const S &s, size_t n)
-{
-    auto it = std::begin(s);
-    // 'advance' the iterator n times
-    std::advance(it, n);
-    return it;
+    return dis(gen) * _max;
 }
 
 int main()
@@ -42,21 +28,21 @@ int main()
     f << total << "\n";
 
     vector<int> randomNumbers, searched, operations;
-    for (int i = 0; i < RANDOM_MAX; i++)
+    for (int i = 0; i < total; i++)
     {
-        int op = (int)(getRandDouble() * 100);
+        int op = getRandInt(100);
         int rand_num;
         if (op < delete_percent && randomNumbers.size() > 0)
         {
             op = 0;
-            swap(randomNumbers[(int)(getRandDouble() * randomNumbers.size())], randomNumbers.back());
+            swap(randomNumbers[getRandInt(randomNumbers.size())], randomNumbers.back());
             rand_num = randomNumbers.back();
             randomNumbers.pop_back();
         }
         else
         {
             op = 1;
-            rand_num = getRandInt();
+            rand_num = getRandInt(RANDOM_MAX);
             randomNumbers.push_back(rand_num);
         }
         f << op << " " << rand_num << "\n";
